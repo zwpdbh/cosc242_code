552d89918ecd74b47e45afa0dfa7b0f0eb0608df
zwpdbh <hyperion_z@outlook.com>
2016-08-27 12:15:14 +1200

    > lab 14

--- lab12_HashTable/lab12_HashTable/main.c
+++ lab12_HashTable/lab12_HashTable/main.c
@@ -3,12 +3,13 @@
 #include "mylib.h"
 #include "htable.h"
 
+/**
 int main(void) {
     htable h = htable_new(113);
     char word[256];
     char op;
     
-    /* We must have a space before the %c */
+    
     while (2 == scanf(" %c %255s", &op, word)) {
         if ('+' == op) {
             htable_insert(h, word);
@@ -17,6 +18,17 @@ int main(void) {
         }
     }
 
+    htable_free(h);
+    return EXIT_SUCCESS;
+}
+ */
+int main(void) {
+    htable h = htable_new(18143);
+    char word[256];
+    while (getword(word, sizeof word, stdin) != EOF) {
+        htable_insert(h, word);
+    }
+    htable_print(h, stdout);
     htable_free(h);
     return EXIT_SUCCESS;
 }
--- lab13_double_hashing/lab13_double_hashing/main.c
+++ lab13_double_hashing/lab13_double_hashing/main.c
@@ -3,21 +3,35 @@
 #include "mylib.h"
 #include "htable.h"
 
-int main(void) {
-    htable h = htable_new(113);
-    char word[256];
-    char op;
-    
-    /* We must have a space before the %c */
-    while (2 == scanf(" %c %255s", &op, word)) {
-        if ('+' == op) {
-            htable_insert(h, word);
-        } else if ('?' == op) {
-            printf("%d %s\n", htable_search(h, word), word);
-        }
-    }
+/**
+ int main(void) {
+ htable h = htable_new(113);
+ char word[256];
+ char op;
+ 
+ 
+ while (2 == scanf(" %c %255s", &op, word)) {
+ if ('+' == op) {
+ htable_insert(h, word);
+ } else if ('?' == op) {
+ printf("%d %s\n", htable_search(h, word), word);
+ }
+ }
+ 
+ htable_free(h);
+ return EXIT_SUCCESS;
+ }
+ */
 
+int main(void) {
+    htable h = htable_new(18143);
+    char word[256];
+    while (getword(word, sizeof word, stdin) != EOF) {
+        htable_insert(h, word);
+    }
+    htable_print(h, stdout);
     htable_free(h);
     return EXIT_SUCCESS;
 }
+
 
